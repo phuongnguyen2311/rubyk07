f9e90a83fefda816722cdc6c92a4c1e7aad99fbc
Hoang Tram <hoangtram13501@gmail.com>
2024-08-05 16:43:01 +0700

    bt1/find the oldest

---
+++ the_oldest.rb
@@ -0,0 +1,28 @@
+class Student
+
+   def initialize
+     @students = []
+   end
+
+   def push(student)
+     @students << student
+   end
+
+   def show
+     puts @students
+   end
+
+   def find_age
+    oldest_student = @students.max_by { |s| s[:age] }
+
+    puts "The oldest:"
+    puts "Name: #{oldest_student[:name]}"
+    puts "Age: #{oldest_student[:age]}"
+    puts "Company: #{oldest_student[:company]}"
+   end
+ end
+  a = Student.new
+ a.push({ name: "Nguyen Van Phuong", age: 32, company: "Neolab" })
+ a.push({ name: "Dau Cong Giap", age: 24, company: "Neolab" })
+ a.push({ name: "Tran Huu Thang", age: 25, company: "Neolab" })
+ a.find_age
